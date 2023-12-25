59ef996031e84d166633c6959cf1722c22b57a8a
bruker <bruker@DESKTOP-25G9I06>
2023-12-25 17:29:41 +0100

    Add project files.

---
+++ WPSwitcher/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WPSwitcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(582, 29);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WPSwitcher";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(531, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(144, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(296, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Switcher do WPKontakt i Spik\'a\r\n";
+            // 
+            // IP
+            // 
+            this.IP.Location = new System.Drawing.Point(148, 183);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(292, 20);
+            this.IP.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(217, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "IP Numeryczne";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(150, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(290, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Nadpisz Adres";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(580, 407);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Switcher";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WPSwitcher/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WPSwitcher
+{
+    public partial class Form1 : Form
+    {
+        bool bWritten = false;
+        string FilePath = @"C:\Windows\System32\drivers\etc\hosts";
+        public Form1()
+        {
+            if(System.IO.File.ReadAllText(FilePath).Contains("jabber.wp.pl"))
+                bWritten = true;
+            InitializeComponent();
+            IP.Text = ParseIP();
+        }
+        bool WriteToFile(string IP)
+        {
+            if(bWritten)
+            {
+                string[] FileLines = System.IO.File.ReadAllLines(FilePath);
+
+                for (int i = 0; i < FileLines.Length; i++)
+                {
+                    if (FileLines[i].Contains("jabber.wp.pl"))
+                    {
+                        FileLines[i] = $"{IP} jabber.wp.pl";
+
+                    }
+
+                }
+                System.IO.File.WriteAllLines(FilePath, FileLines);
+                return false;
+            } else
+            {
+                System.IO.File.AppendAllText(FilePath, $"{IP} jabber.wp.pl");
+                return true;
+            }
+        }
+        string ParseIP()
+        {
+            string[] FileLines = System.IO.File.ReadAllLines(FilePath);
+
+            for (int i = 0; i < FileLines.Length; i++)
+            {
+                if (FileLines[i].Contains("jabber.wp.pl"))
+                {
+                    string[] splitted = FileLines[i].Split(' ');
+                    return splitted[0];
+
+                }
+
+            }
+            return "";
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WriteToFile(IP.Text);
+            MessageBox.Show("Nadpisano plik pomyślnie");
+        }
+    }
+}
